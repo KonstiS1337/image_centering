02a1aa1b9b60b150a557049b4bbbd672a0d4e49f
Konstantin Stute <konstantin.stute@stud.tu-darmstadt.de>
2024-09-03 09:45:44 +0200

    changed pid controller class

--- include/image_centering/image_centering.hpp
+++ include/image_centering/image_centering.hpp
@@ -1,6 +1,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/int16.hpp>
-#include <control_toolbox/pid_ros.hpp>
+#include <std_msgs/msg/float32.hpp>
+#include <image_centering/pid.hpp>
 #include <epuck_driver_interfaces/srv/change_robot_state.hpp>
 #include <std_srvs/srv/set_bool.hpp>
 
@@ -12,16 +13,23 @@ class ImageCentering : public rclcpp::Node {
         ImageCentering();
     private:
         int16_t current_x_;
+        float pid_output_;
 
         rclcpp::Subscription<std_msgs::msg::Int16>::SharedPtr coords_sub_;
+        rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pid_output_pub_;
         rclcpp::Client<epuck_driver_interfaces::srv::ChangeRobotState>::SharedPtr robot_control_srv_;
         rclcpp::TimerBase::SharedPtr timer_;
-        control_toolbox::PidROS pid_;
+        std::shared_ptr<PIDController<float>> pid_;
         rclcpp::Time pid_last_call_;
-        rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr activate_srv_;
+        rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr activate_srv_,pid_p_srv_,pid_i_srv_,pid_d_srv_;
 
+        unsigned long pidTimeFunction();
         void coordsSub(const std_msgs::msg::Int16::ConstPtr & data);
         void controlLoop();
         void srvCB(const std::shared_ptr<std_srvs::srv::SetBool::Request> req, std::shared_ptr<std_srvs::srv::SetBool::Response> resp);
+        //only needed for parameter tuning
+        void srvCBp(const std::shared_ptr<std_srvs::srv::SetBool::Request> req, std::shared_ptr<std_srvs::srv::SetBool::Response> resp);
+        void srvCBi(const std::shared_ptr<std_srvs::srv::SetBool::Request> req, std::shared_ptr<std_srvs::srv::SetBool::Response> resp);
+        void srvCBd(const std::shared_ptr<std_srvs::srv::SetBool::Request> req, std::shared_ptr<std_srvs::srv::SetBool::Response> resp);
 
 };
--- src/image_centering.cpp
+++ src/image_centering.cpp
@@ -1,8 +1,7 @@
 #include "image_centering/image_centering.hpp"
 
 ImageCentering::ImageCentering() : Node("image_centering_node"),
-                                    pid_last_call_(builtin_interfaces::msg::Time()),
-                                    pid_(this->get_node_base_interface(),this->get_node_logging_interface(),this->get_node_parameters_interface(),this->get_node_topics_interface(),std::string("image_center_pid"))
+                                    pid_last_call_(builtin_interfaces::msg::Time())
                                     {
     this->declare_parameter<std::string>("epuck_name","epuck");
     std::string name = this->get_parameter("epuck_name").as_string();
@@ -10,7 +9,43 @@ ImageCentering::ImageCentering() : Node("image_centering_node"),
     timer_ = this->create_wall_timer(std::chrono::milliseconds(100),std::bind(&ImageCentering::controlLoop,this));
     activate_srv_ = this->create_service<std_srvs::srv::SetBool>(name + "/image_centering/activation",std::bind(&ImageCentering::srvCB,this,std::placeholders::_1,std::placeholders::_2));
     robot_control_srv_ = this->create_client<epuck_driver_interfaces::srv::ChangeRobotState>(name + "/robot_control");
+    pid_output_pub_ = this->create_publisher<std_msgs::msg::Float32>(name + "/pid_output",1);
+    // setting up pidsetTarget
+    pid_ = std::make_shared<PIDController<float>>(1,0,0,
+    [this]() {
+        return current_x_;
+    },
+    [this] (float output) {
+        pid_output_ = output;
+        return;
+    });
 
+    pid_->registerTimeFunction([this]() -> unsigned long {
+    return this->pidTimeFunction();
+    });
+
+    pid_->setTarget(0);
+    pid_->setEnabled(true);
+}
+
+void ImageCentering::srvCBp(const std::shared_ptr<std_srvs::srv::SetBool::Request> req, std::shared_ptr<std_srvs::srv::SetBool::Response> resp) {
+    pid_->setP(req->data);
+    resp->success = true;
+    return;
+}
+void ImageCentering::srvCBi(const std::shared_ptr<std_srvs::srv::SetBool::Request> req, std::shared_ptr<std_srvs::srv::SetBool::Response> resp){
+    pid_->setI(req->data);
+    resp->success = true;
+    return;
+}
+void ImageCentering::srvCBd(const std::shared_ptr<std_srvs::srv::SetBool::Request> req, std::shared_ptr<std_srvs::srv::SetBool::Response> resp){
+    pid_->setD(req->data);
+    resp->success = true;
+    return;
+}
+
+unsigned long ImageCentering::pidTimeFunction() {
+    return (unsigned long) this->now().nanoseconds() / 1e+6;
 }
 
 void ImageCentering::coordsSub(const std_msgs::msg::Int16::ConstPtr & data) {
@@ -20,19 +55,21 @@ void ImageCentering::coordsSub(const std_msgs::msg::Int16::ConstPtr & data) {
 
 void ImageCentering::controlLoop() {
     rclcpp::Time current_time = this->now();
-    double controller_output = pid_.computeCommand(static_cast<double>(current_x_),current_time - pid_last_call_);
-    pid_last_call_ = current_time;
+    pid_->tick();
+    std_msgs::msg::Float32 msg;
+    msg.data = pid_output_;
+    pid_output_pub_->publish(msg);
     // Sending command
     auto request_left = std::make_shared<epuck_driver_interfaces::srv::ChangeRobotState::Request>();
     auto request_right = std::make_shared<epuck_driver_interfaces::srv::ChangeRobotState::Request>();
     request_left->module = request_left->MODULE_LEFT_MOTOR;
     request_right->module = request_right->MODULE_RIGHT_MOTOR;
 
-    int turn_speed = controller_output > 100 ? TURN_SPEED: //TODO adjust values to reasonable values
-                                                            controller_output > 50 ? TURN_SPEED * 0.5 : 
-                                                                controller_output <= 20 ? 0 : TURN_SPEED * 0.25;
+    int turn_speed = pid_output_ > 100 ? TURN_SPEED: //TODO adjust values to reasonable values
+                                                            pid_output_ > 50 ? TURN_SPEED * 0.5 : 
+                                                                pid_output_ <= 20 ? 0 : TURN_SPEED * 0.25;
 
-    if(controller_output >= 0) {
+    if(pid_output_ >= 0) {
         request_left->value = turn_speed;
         request_right->value = -turn_speed;
     }
@@ -51,6 +88,7 @@ void ImageCentering::srvCB(const std::shared_ptr<std_srvs::srv::SetBool::Request
     if(req->data) {
         current_x_ = 0;
         timer_->reset();
+        pid_->setEnabled(true);
     } 
     else {
         timer_->cancel();
@@ -62,7 +100,7 @@ void ImageCentering::srvCB(const std::shared_ptr<std_srvs::srv::SetBool::Request
         robot_control_srv_->async_send_request(request_left);
         robot_control_srv_->async_send_request(request_right);
         //reseting pid
-        pid_.reset();
+        pid_->setEnabled(false);
     }
     resp->success = true;
     return;
